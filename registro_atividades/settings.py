edff06fda59b5ed47f6263fc1bae8f9ef0475314
Igor Julliano <igor.julliano2@gmail.com>
2023-12-17 20:51:31 -0300

    feat: add static files and fix typos

--- registro_atividades/settings.py
+++ registro_atividades/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dm40-1dai06wa)bcu)@37j%lz=s!)vnw_%@xv8b@w&qd_x(cee'
+SECRET_KEY = "django-insecure-dm40-1dai06wa)bcu)@37j%lz=s!)vnw_%@xv8b@w&qd_x(cee"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,85 +37,87 @@ CSRF_TRUSTED_ORIGINS = ["https://registros-uab.fly.dev", "https://*.127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'django_bootstrap5',
-    'scholar.apps.ScholarConfig',
-    'scholarship.apps.ScholarshipConfig',
-    'records.apps.RecordsConfig',
-    'users.apps.UsersConfig',
-    'coordinator.apps.CoordinatorConfig'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.humanize",
+    "django_bootstrap5",
+    "scholar.apps.ScholarConfig",
+    "scholarship.apps.ScholarshipConfig",
+    "records.apps.RecordsConfig",
+    "users.apps.UsersConfig",
+    "coordinator.apps.CoordinatorConfig",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
-ROOT_URLCONF = 'registro_atividades.urls'
+ROOT_URLCONF = "registro_atividades.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'registro_atividades.wsgi.application'
+WSGI_APPLICATION = "registro_atividades.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.dj_db_url("DATABASE_URL", default="postgres://myuser:secret@localhost:5432/registros_uab?sslmode=disable"),
+    "default": env.dj_db_url(
+        "DATABASE_URL",
+        default="postgres://myuser:secret@localhost:5432/registros_uab?sslmode=disable",
+    ),
 }
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.CustomUser"
 
-LOGIN_REDIRECT_URL='/registros'
-LOGIN_URL='/login'
+LOGIN_REDIRECT_URL = "/registros"
+LOGIN_URL = "/login"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -123,19 +125,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-BR'
-TIME_ZONE = 'America/Fortaleza'
-TIME_INPUT_FORMATS = ('%H:%M',)
+LANGUAGE_CODE = "pt-BR"
+TIME_ZONE = "America/Fortaleza"
+TIME_INPUT_FORMATS = ("%H:%M",)
 
 USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = "static/"
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = BASE_DIR / "productionfiles"
 BOOTSTRAP5_INCLUDE_JS = True
 
-# Phone number field 
+# Add this in your settings.py file:
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-PHONENUMBER_DEFAULT_REGION='BR'
+# Phone number field
+
+PHONENUMBER_DEFAULT_REGION = "BR"
